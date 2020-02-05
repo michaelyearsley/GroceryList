bf231e9adab75bda4c14441ea3a7d0b956e39f42
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-02-04 17:24:09 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GroceryList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grocerylist = new List<GroceryList>();
+
+
+            bool adding = true;
+            while (adding)
+            {
+                var newGroceryList = new GroceryList();
+
+                newGroceryList.Item = support.Ask("Enter items: ");
+
+                newGroceryList.Price = double.Parse(support.Ask("Enter price: "));
+
+                newGroceryList.Store = support.Ask("Enter store: ");
+
+                grocerylist.Add(newGroceryList);
+
+                adding = "n" != support.Ask("do you want to enter anot item (y/n)? ");
+
+            }
+
+            foreach (var grocery in grocerylist)
+            {
+                Console.WriteLine("Item: {0} Price:{1} Store: {2}", grocery.Item, grocery.Price, grocery.Store);
+                Console.ReadLine();
+            }
+
+        }
+    }
+    class GroceryList
+    {
+        public string Item;
+        public double Price;
+        public string Store;
+    }
+
+}
---
+++ support.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroceryList
+{
+    class support
+    {
+        static public string Ask(string question)
+        {
+            System.Console.Write(question);
+            return System.Console.ReadLine();
+        }
+        class GroceryList
+        {
+            public string Item;
+            public double Price;
+            public string Store;
+        }
+    }
+}
