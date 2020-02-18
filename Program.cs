ddce5b841d71fac82d005dfad3b6c6e2a59bd94e
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-02-17 23:27:02 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,8 +11,6 @@ namespace GroceryList
             //System.Collections.Generic 1
             var grocerylist = new List<GroceryList>();
 
-
-
             // system 1
             bool adding = true;
             // system 2
@@ -25,22 +23,30 @@ namespace GroceryList
                 newGroceryList.Price = double.Parse(support.Ask("Enter price: "));
 
                 newGroceryList.Store = support.Ask("Enter store: ");
-
+                //System.Collections.Generic 2
                 grocerylist.Add(newGroceryList);
-                
-
-                adding = "n" != support.Ask("do you want to enter anot item (y/n)? ");
-
+                //System 3 = ToLower 
+                //System 4 = ToChar
+                adding = 'n' != Char.ToLower(Convert.ToChar(support.Ask("do you want to enter anot item (y/n)? ")));
             }
-
             //System.Linq 1
+            double total = grocerylist.Sum(x => x.Price);
+            //System.Linq 2
+            double average = grocerylist.Average(x => x.Price);
+            //System.Linq 3
+            double max = grocerylist.Max(x => x.Price);
+            //System.Linq 4
+            int count = grocerylist.Count();
+            //System.Linq 5
             grocerylist = grocerylist.OrderBy(x => x.Item).ToList();
-            //system 3
+            
+
+            //system 5
             foreach (var grocery in grocerylist)
             {
                 Console.WriteLine("Item: {0} Price:{1} Store: {2}", grocery.Item, grocery.Price, grocery.Store);
-                
             }
+            Console.WriteLine("you have {0} items with totatl price {1} and  an average price {2} and the max priced item was {3}",count ,total ,average, max);
             Console.ReadLine();
         }
     }
